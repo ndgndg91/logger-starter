1bbc8973a1d79a3fffe9d460b2f6734674ccf687
ndgndg91 <ndgndg91@gamil.com>
2024-03-24 18:07:42 +0900

    <feat> add @Conditional... annotations

--- src/main/kotlin/com/ndgndg91/logger/HttpLoggingAutoConfiguration.kt
+++ src/main/kotlin/com/ndgndg91/logger/HttpLoggingAutoConfiguration.kt
@@ -1,15 +1,19 @@
 package com.ndgndg91.logger
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ConditionalOnProperty(value = ["logging.http.enabled"], havingValue = "true", matchIfMissing = false)
 @ConditionalOnWebApplication
 class HttpLoggingAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     fun loggingFilter(): FilterRegistrationBean<HttpLoggingFilter> {
         val registrationBean: FilterRegistrationBean<HttpLoggingFilter> = FilterRegistrationBean()
         registrationBean.filter = HttpLoggingFilter()
